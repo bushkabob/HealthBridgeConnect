de3cfdc1dfc3f75eae9c8f1000650b52efcfdf37
Josh <59369632+bushkabob@users.noreply.github.com>
2025-10-19 18:33:38 -0400

    add location

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,41 @@
 import DraggableSearchBar from '@/components/DraggableSearchBar';
-import { View } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+import { GlassView } from 'expo-glass-effect';
+import * as Location from 'expo-location';
+import { useEffect, useRef } from 'react';
+import { Pressable, View } from "react-native";
 import MapView from 'react-native-maps';
 
 export default function Map() {
+    const mapRef = useRef<MapView>(null);
+
+    const moveToLocation = (location: Location.LocationObject) => {
+        if (mapRef.current) {
+            mapRef.current.animateToRegion({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01,
+            }, 1000);
+        }
+    }
+
+    async function getCurrentLocation(callback?: (location: Location.LocationObject) => void) {
+        
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+            return;
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        callback && callback(location);
+    }
+
+    useEffect(() => {
+        getCurrentLocation();
+    }, []);
+
+
     return (
         <View
             style={{
@@ -11,8 +44,14 @@ export default function Map() {
                 alignItems: "center",
             }}
         >
-            <MapView style={{ width: '100%', height: '100%' }} />
-            <DraggableSearchBar />
+            <MapView mapPadding={{ top: 50, bottom:50, left: 20, right:20 }} ref={mapRef} style={{ width: '100%', height: '100%', }} showsMyLocationButton showsUserLocation />
+            <DraggableSearchBar>
+                <GlassView isInteractive style={{ padding: 10, borderRadius: 100, alignSelf: "flex-end"}} >
+                    <Pressable onPress={() => { getCurrentLocation(moveToLocation) }} >
+                        <Ionicons name="navigate" size={30} color="gray" />
+                    </Pressable>
+                </GlassView>
+            </DraggableSearchBar>
         </View>
     );
 }
--- components/DraggableSearchBar.tsx
+++ components/DraggableSearchBar.tsx
@@ -5,10 +5,14 @@ import { GlassContainer, GlassView } from "expo-glass-effect";
 import React from "react";
 import { Dimensions, Keyboard, Pressable, View } from "react-native";
 import { Gesture, GestureDetector, GestureHandlerRootView, TextInput } from "react-native-gesture-handler";
-import Animated, { Easing, useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
+import Animated, { Easing, interpolate, useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
 import { runOnJS } from "react-native-worklets";
 
-const DraggableSearchBar: React.FC = () => {
+type DraggableSearchBarProps = {
+	children?: React.ReactNode;
+}
+
+const DraggableSearchBar: React.FC<DraggableSearchBarProps> = ({children}) => {
 	const TextInputRef = React.useRef<TextInput>(null);
 
 	const { height, width } = Dimensions.get("window");
@@ -19,7 +23,7 @@ const DraggableSearchBar: React.FC = () => {
     	easing: Easing.bezier(0.5, 0.01, 0, 1),
 	};
 
-	const minHeight = 60;
+	const minHeight = 65;
 	const viewHeight = useSharedValue(minHeight);
 
 	function dismissKeyboard () {
@@ -58,28 +62,43 @@ const DraggableSearchBar: React.FC = () => {
 		};
 	});
 
+	const animatedChildrenStyles = useAnimatedStyle(() => {
+		return {
+			opacity: interpolate(viewHeight.value, [maxHeight / 2, 3 * maxHeight / 4], [1, 0]),
+			bottom: viewHeight.value + 30 - 20 * (viewHeight.value - minHeight) / (maxHeight - minHeight),
+			left: 20 - 20 * (viewHeight.value - minHeight) / (maxHeight - minHeight),
+			right: 20 - 20 * (viewHeight.value - minHeight) / (maxHeight - minHeight),
+			pointerEvents: viewHeight.value <= maxHeight / 2 ? "box-none" : "none"
+		};
+	});
 
 	return (
-			<GestureHandlerRootView style={{flex: 1, width: "100%"}} >
-				<GestureDetector gesture={panGesture}>
-					<Animated.View style={[{ position: 'absolute',
-						borderRadius: 100
-						}, animatedStyles]}
-					>
-						<GlassContainer>
-							<GlassView style={{width: "100%", alignItems: "center", height: "100%", borderRadius: 100}} >
-								<View style={{backgroundColor: "#a5adb0", height: 4, marginHorizontal: 10, marginVertical: 4, borderRadius: 20, width: 50}} />
-								<Pressable onPress={()=>TextInputRef.current?.focus()} style={{alignSelf: "stretch"}} >
-									<GlassView isInteractive tintColor="#d3d3d3" style={{ flexDirection: "row", alignSelf: "stretch", alignItems: "center", height: 40, marginHorizontal: 10, marginBottom: 10, borderRadius: 80}} >
-										<Ionicons name="search" size={20} color="gray" style={{ marginLeft: 10, marginRight: 5 }} />
-									<TextInput onFocus={()=>viewHeight.value = withTiming(maxHeight, config)} ref={TextInputRef} placeholder="Search FQHCs" placeholderTextColor="gray" style={{width: "100%"}} />
+			<>
+				<Animated.View style={[{position: "absolute", marginHorizontal: 10}, animatedChildrenStyles]} >
+					{children}
+				</Animated.View>
+				<GestureHandlerRootView style={{flex: 1, width: "100%"}} >
+					<GestureDetector gesture={panGesture}>
+						<Animated.View style={[{ position: 'absolute',
+							borderRadius: 100
+							}, animatedStyles]}
+						>
+							<GlassContainer>
+								<GlassView style={{width: "100%", alignItems: "center", height: "100%", borderRadius: 100}} >
+									<View style={{backgroundColor: "#a5adb0", height: 4, marginHorizontal: 10, marginVertical: 4, borderRadius: 20, width: 50}} />
+									<Pressable onPress={()=>TextInputRef.current?.focus()} style={{alignSelf: "stretch"}} >
+										<GlassView isInteractive tintColor="#d3d3d3" style={{ flexDirection: "row", alignSelf: "stretch", alignItems: "center", height: 40, marginHorizontal: 10, marginVertical: 2, borderRadius: 80}} >
+											<Ionicons name="search" size={20} color="gray" style={{ marginLeft: 10, marginRight: 5 }} />
+										<TextInput onFocus={()=>viewHeight.value = withTiming(maxHeight, config)} ref={TextInputRef} placeholder="Search FQHCs" placeholderTextColor="gray" style={{width: "100%"}} />
+									</GlassView>
+									</Pressable>
 								</GlassView>
-								</Pressable>
-							</GlassView>
-						</GlassContainer>
-					</Animated.View>
-				</GestureDetector>
-			</GestureHandlerRootView>
+							</GlassContainer>
+
+						</Animated.View>
+					</GestureDetector>
+				</GestureHandlerRootView>
+			</>
 	)
 }
 
